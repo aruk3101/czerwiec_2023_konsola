74efe8b05f97d48b48550b1c71d3af1333cae5a5
Damian Raczek <draczekprojekt@gmail.com>
2024-10-08 10:15:22 +0200

    Bubble sort function created

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@
         {
             int[] randomNumbers = GenerateRandomNumbers(100);
 
-           
+            BubbleSort(randomNumbers);
 
             Console.WriteLine("Tablica po sortowaniu:");
             Console.WriteLine(string.Join(", ", randomNumbers));
@@ -23,6 +23,23 @@
             }
 
             return numbers;
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            int length = array.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                }
+            }
         }
 
     }
