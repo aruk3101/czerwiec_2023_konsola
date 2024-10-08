b90fb08aeb1b729cbf5b06016ef6c4f86ffd6771
Damian Raczek <draczekprojekt@gmail.com>
2024-10-08 10:15:05 +0200

    Function for generating random numbers created

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            int[] randomNumbers = GenerateRandomNumbers(100);
+
+           
+
+            Console.WriteLine("Tablica po sortowaniu:");
+            Console.WriteLine(string.Join(", ", randomNumbers));
         }
+
+        static int[] GenerateRandomNumbers(int size)
+        {
+            Random random = new Random();
+            int[] numbers = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next(1, 101);
+            }
+
+            return numbers;
+        }
+
     }
 }
